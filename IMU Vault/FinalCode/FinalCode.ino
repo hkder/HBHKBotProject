9b009524bd3ea706a6610fb4f8883ae49c531219
Hyungbin Jun <hyungbin.jun@duke.edu>
2019-11-12 18:42:34 -0500

    edited final code; added password change feature

--- IMU Vault/FinalCode/FinalCode.ino
+++ IMU Vault/FinalCode/FinalCode.ino
@@ -8,27 +8,28 @@ Servo myservo;
 int status;
 const int servo = 8;
 float accx, accy, accz;
-unsigned long startmillis;
-unsigned long currmillis;
-const unsigned long period = 5000;
 const int ledPin = 13;
-boolean timeStart_init = false;
-boolean initStart[11];
-boolean locked = true;
-
 int threshold = 1;
+int delayTime = 2000;
 
-const int passwordSize = 4;
-int passcode[] = {0, 0, 0, 0};
-int password[passwordSize];
-int currentplace = 0;
+int correctPassword[] = {0,0,0,0};
+int passwordSize = 4;
+int password[] = {-1, -1, -1, -1};
+int currentPlace = 0;
+int newPassword[] = {-1, -1, -1, -1};
+int currentPlaceForNewPassword = 0;
+
+boolean passwordChangeMode = false;
+boolean locked = true;
 
 int returnLocationNumber(float accx, float accy, float accz){
   float myXacc[] = {-6.59, -9.24, -8.17, -3.80, 2.16, 7.51, 10.23, 9.12, 4.79, -1.19};
   float myYacc[] = {7.48, 2.01, -3.93, -8.10, -8.95, -6.13, -0.63, 5.42, 9.55, 10.40};
+  // when facing the sky
   if(accz <= -8.0){
     return 10;
   }
+  // when facing the ground
   else if (accz >= 8.0){
     return 11;
   }
@@ -65,12 +66,6 @@ int returnLocationNumber(float accx, float accy, float accz){
   return 99;
 }
 
-void baseInitStart(){
-  for(int i = 0; i < 11; i++){
-    initStart[i] = false;
-  }
-}
-
 void setup()
 {
   myservo.attach(servo);
@@ -87,92 +82,159 @@ void setup()
     Serial.println(status);
     while(1) {}
   }
-  startmillis = millis();
 }
 
 void loop()
 {
-  IMU.readSensor();
-  accx = IMU.getAccelX_mss();
-  accy = IMU.getAccelY_mss();
-  accz = IMU.getAccelZ_mss();
-  //Serial.println(accx);
-  //Serial.println(accy);
-  //Serial.println(accz);
-  int location = returnLocationNumber(accx, accy, accz);
-  //Serial.println(location);
-
-  //go into reading mode
-  if(0<=location && 9>=location){
-    startTimer(location);
-  }
-  if(!locked && location == 10){
-    startTimer(location);
-  }
-}
-
-void startTimer(int location){
-  currmillis = millis();
-  if(initStart[location] == false){
-      baseInitStart();
-      initStart[location] = true;
-      startmillis = millis();
-      delay(50);
-    } else{
-      if(currmillis - startmillis >= period){
-        //Serial.println("5 second passed! in position ");
-        Serial.println(location);
-        Serial.println(currentplace);
-        initStart[location] = false;
-        //add it to the array;
-        writepassword(location);
-      }
-      //Serial.println("inside position");
-      //Serial.print(location);
+  int location = getLocation();
+  if(!locked) {
+    if(!passwordChangeMode && location==11) {
+      // go into password ChangeMode
+      toPasswordChangeMode();
     }
+  
+    else if(passwordChangeMode && location==10) {
+      // save new password. If fails, reset written new password
+      saveNewPassword();
+    }
+  
+    else if(passwordChangeMode && location >=0 && location <= 9) {
+      // fill in new password
+      writeNewPassword(location);
+    }
+    else if(location==0) {
+      // lock the door
+      lock();
+    }
+  }
+  else if(location>=0 && location<=9) {
+    // fill in password
+    writePassword(location);
+  }
+  else if(location==10) {
+    // try to unlock the door. If fails, reset written password
+    unlock();
+  }
+  else if(location==11) {
+    delay(delayTime);
+  }
 }
 
-void lock(){
+boolean stayedLongEnough(int location){
+  for(int i=0; i<100; i++) {
+    delay(10);
+    int currentLocation = getLocation();
+    if(location != currentLocation) {
+      return false;
+    }
+  }
+  Serial.print("stayed long enough at ");
+  Serial.println(location);
+  return true;
+}
+
+void toPasswordChangeMode() {
+  passwordChangeMode = true;
+  for(int i=0; i<passwordSize; i++) {
+    newPassword[i] = -1;
+  }
+  currentPlaceForNewPassword = 0;
+  Serial.println("now in password change mode");
+}
+
+void saveNewPassword() {
+  if(currentPlaceForNewPassword ==4) {
+    for(int i=0; i<passwordSize; i++) {
+      correctPassword[i] = newPassword[i];
+    }
+    for(int i=0; i<passwordSize; i++) {
+      newPassword[i] = -1;
+    }
+    currentPlaceForNewPassword = 0;
+    passwordChangeMode = false;
+    Serial.println("saving the new password and resetting the written new password");
+  }
+}
+
+void writeNewPassword(int location) {
+  if(stayedLongEnough(location) && currentPlaceForNewPassword < 4) {
+    newPassword[currentPlaceForNewPassword] = location;
+    currentPlaceForNewPassword = currentPlaceForNewPassword + 1;
+    Serial.print("writing new password. current length of new password is ");
+    Serial.println(currentPlaceForNewPassword);
+    delay(delayTime);
+  }
+}
+
+void lock() {
+  if(stayedLongEnough(0)) {
+    Serial.println("locking the door");
+    lockServo();
+    delay(delayTime);
+  }
+}
+
+void writePassword(int location) {
+  if(stayedLongEnough(location) && currentPlace < 4) {
+    password[currentPlace] = location;
+    currentPlace = currentPlace + 1;
+    Serial.print("writing password. current length of written password is ");
+    Serial.println(currentPlace);
+    delay(delayTime);
+  }
+}
+
+void unlock() {
+  if(stayedLongEnough(10)) {
+    //password check and open if correct
+    boolean correct = true;
+    for (int i = 0; i<passwordSize; i++) {
+      if(password[i] != correctPassword[i]) {
+        correct = false;
+        break;
+      }
+    }
+    if(correct) {
+      Serial.println("password correct, unlocking the door and resetting the written password");
+      for(int i=0; i<passwordSize; i++) {
+        password[i] = -1;
+      }
+      currentPlace = 0;
+      unlockServo();
+      delay(delayTime);
+    }
+    else {
+      // password is wrong or incomplete, reset the writen password
+      Serial.println("password incorrect, resetting the written password");
+      for(int i=0; i<passwordSize; i++) {
+        password[i] = -1;
+      }
+      currentPlace = 0;
+      delay(delayTime);
+    }
+  }
+}
+
+void lockServo(){
   myservo.write(70);
   delay(915);
   myservo.write(90);
   locked = true;
 }
 
-void unlock(){
+void unlockServo(){
   myservo.write(110);
   delay(1000);
   myservo.write(90);
   locked = false;
 }
 
-void trytoopen(int place){
-  if(place == 4) {
-    boolean match = true;
-    for(int i=0; i<passwordSize; i++){
-      if(password[i] != passcode[i]) {
-        match = false;
-        break;
-      }
-      else {
-        Serial.println("check");
-      }
-    }
-    if (match) {
-      Serial.println("unlock");
-      unlock();
-    }
-    currentplace = 0;
-  }
-}
-
-void writepassword(int number) {
-  if(!locked && number == 10) {
-    lock();
-  }
-  password[currentplace]=number;
-  currentplace = currentplace + 1;
-  trytoopen(currentplace);
+int getLocation() {
+  IMU.readSensor();
+  accx = IMU.getAccelX_mss();
+  accy = IMU.getAccelY_mss();
+  accz = IMU.getAccelZ_mss();
+  return returnLocationNumber(accx, accy, accz);
 }
 
 
